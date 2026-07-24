dfa2119896ea1d46ed367c2ed00d46c75fd3d252
hoangnhat204 <hoangnhat06102004@gmail.com>
2026-07-25 03:41:48 +1000

    update website

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(session({
     name: 'session',
     keys: [process.env.SESSION_SECRET || 'my-secret-key-123'],
